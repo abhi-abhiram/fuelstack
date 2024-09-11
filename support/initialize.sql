0062ee775675437949fa8ffe1b60af2ad9d0deed
Abhiram <anukoolaabhiram@outlook.com>
2024-09-11 15:50:17 +0530

    removed some files

--- support/initialize.sql
+++
@@ -1,7 +0,0 @@
-SET timezone='UTC';
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- CREATE USER dbmaster with encrypted password 'securedb';
--- DROP DATABASE IF EXISTS fuelstack;
--- CREATE DATABASE fuelstack;
--- GRANT ALL PRIVILEGES ON DATABASE fuelstack TO dbmaster;
